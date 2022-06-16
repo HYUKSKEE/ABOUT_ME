17f7f8b6efa47a444142ea6cb261e69e0b4ef76a
HYUKSKEE <gin280833@gmail.com>
2022-06-16 14:26:06 +0900

    06/16 Album JS 수정중

--- Javascript/Album.js
+++ Javascript/Album.js
@@ -16,7 +16,3 @@ musicPlayItems.forEach(function (item) {
     item.isPlaying = !item.isPlaying;
   });
 });
-
-function new(){
- window.open("test.asp","new", "width=300, height=300, left=30, top=30, scrollbars=no,titlebar=no,status=no,resizable=no,fullscreen=no");
-}
