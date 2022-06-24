3b8c5d1b3e4c73434a190aad9f51ec507668f98f
HYUKSKEE <gin280833@gmail.com>
2022-06-24 19:28:37 +0900

    06/24 Contact page 완성

--- Javascript/Album.js
+++ Javascript/Album.js
@@ -1,1 +1,16 @@
+const musicPlayItems = document.querySelectorAll(".music-play-item");
+musicPlayItems.forEach(function (item) {
+  item.isPlaying = false;
+  item.audio = item.querySelector("audio");
+  item.addEventListener("click", function () {
+    if (this.isPlaying) {
+      item.audio.pause();
+      item.audio.currentTime = 0;
+    } else {
+      item.audio.play();
+    }
 
+    console.log(this.isPlaying);
+    item.isPlaying = !item.isPlaying;
+  });
+});
