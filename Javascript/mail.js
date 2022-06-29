c7f8dda3d2ca241b56801a173c75f97bc0e90e36
HYUKSKEE <gin280833@gmail.com>
2022-06-29 23:30:22 +0900

    06/29 mail.js 유효성검사 수정중

--- Javascript/mail.js
+++ Javascript/mail.js
@@ -8,27 +8,29 @@ document.addEventListener("DOMContentLoaded", function () {
       email: document.getElementById("email").value,
       message: document.getElementById("message").value,
     };
-    console.log(Object.values(templateParams));
-    function validEmail() {
-      const nameValue = templateParams.value;
-      const pattern =
-        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-      const emailValue = templateParams.email.match(pattern);
-      if (emailValue == null || nameValue == null) {
-        alert("입력란을 올바르게 입력해주세요.");
-        window.location.reload();
-      }
-    }
-    validEmail();
-    emailjs.send("service_4kpx5bq", "template_5x64z8d", templateParams).then(
-      function (response) {
-        alert("SUCCESS!", response.status, response.text);
-        window.location.reload();
-      },
-      function (error) {
-        alert("FAILED...", error);
-        window.location.reload();
-      }
-    );
+
+    emailjs
+      .send("service_4kpx5bq", "template_5x64z8d", templateParams)
+      .then(function (response, error) {
+        const pattern =
+          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        const emailValue = templateParams.email.match(pattern);
+        const nameValue = templateParams.value;
+        if (emailValue !== null && nameValue !== null) {
+          alert("SUCCESS!", response.status, response.text);
+        } else {
+          alert("FAILED...", error);
+        }
+      });
+    /* .then(
+        function (response) {
+          alert("SUCCESS!", response.status, response.text);
+          window.location.reload();
+        },
+        function (error) {
+          alert("FAILED...", error);
+          window.location.reload();
+        }
+      ); */
   });
 });
