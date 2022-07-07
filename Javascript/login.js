cd07bb355eca750839e895e16bf6dec29412fd59
HYUKSKEE <gin280833@gmail.com>
2022-07-07 20:34:38 +0900

    07/07 html js 연동 에러 처리중

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,34 +1,30 @@
-"use strict";
 const loginForm = document.querySelector("#Login-form");
 const loginInput = document.querySelector("#Login-form input");
-
-const USERNAME_KEY = "username";
+const mainDiv = document.getElementById("main");
+const answers = todaysQuiz.answer;
+const ANSWER_KEY = "answer";
 const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
-  const username = loginInput.value;
-  if (username != "hyukskee") {
+  const currentAnswer = loginInput.value;
+  if (currentAnswer != answers) {
     event.preventDefault();
     alert("암호가 틀리다!");
   } else {
     event.preventDefault();
-    localStorage.setItem(USERNAME_KEY, username);
+    localStorage.setItem(ANSWER_KEY, currentAnswer);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    goToLink.classList.remove(HIDDEN_CLASSNAME);
-    toDoSection.classList.remove(HIDDEN_CLASSNAME);
-    toDoFormShow.classList.remove(HIDDEN_CLASSNAME);
+    mainDiv.classList.remove(HIDDEN_CLASSNAME);
   }
 }
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-if (savedUsername === null) {
+const savedAnswer = localStorage.getItem(ANSWER_KEY);
+if (savedAnswer === null) {
   // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-} else if (savedUsername == "hyukskee") {
+} else if (savedAnswer == answers) {
   // show the greeting
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  goToLink.classList.remove(HIDDEN_CLASSNAME);
-  toDoSection.classList.remove(HIDDEN_CLASSNAME);
-  toDoFormShow.classList.remove(HIDDEN_CLASSNAME);
+  mainDiv.classList.remove(HIDDEN_CLASSNAME);
 }
 
 /* function paintGreetings() {
